cc99c8058426b2c75281822dc009b4e6eb79a18f
Shidomo <shindor.hardcore@gmail.com>
2023-03-07 17:41:10 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import {v4 as uuidv4} from 'uuid';
+import { randomColor} from 'randomcolor'
+import Draggable from 'react-draggable'
+
+function App() {
+  const [item, setItem] = useState('')
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('items')) || []
+  )
+
+    useEffect(() => {
+      localStorage.setItem('items',JSON.stringify(items))
+    },[items])
+
+    const newItem = () => {
+      if(item.trim() !== ''){
+        const newItem = {
+          id: uuidv4(),
+          item,
+          color: randomColor({
+            luminosity: 'light'
+          }),
+          defaultPos: {
+            x: 500,
+            y: -500
+          }
+        }
+        setItems((items) => [...items, newItem])
+        setItem('')
+      }else {
+        alert('Enter something...')
+      }
+    }
+
+    const deleteNode = (id) => {
+      setItems(items.filter((item) => item.id !== id))
+    }
+
+
+    const updatePos = (data, index) => {
+      let newArray = [...items]
+      newArray[index].defaultPos = {x: data.x, y:data.y}
+      setItems(newArray)
+    } 
+
+    const keyPress = (e) => {
+      const code = e.which
+      if(code === 13 ){
+        newItem()
+      }
+    }
+
+  return (
+    <div className="App">
+      <div className='wrapper'>
+        <input type='text' placeholder='Enter something' onChange={(e) => setItem(e.target.value)} value={item} onKeyPress={(e)=> keyPress(e)}/>
+        <button className='enter' onClick={newItem}>ENTER</button>
+      </div>
+
+      {
+        items.map((item, index) => {
+          return(
+            <Draggable
+              key={index}
+              defaultPosition={item.defaultPos}
+              onStop={(_, data) => 
+              updatePos(data, index)
+              }
+            >
+
+              <div className='todo__item' style={{backgroundColor:item.color}}>
+                {`${item.item}`}
+                <button className='delete' onClick={() => deleteNode(item.id)}>
+                  X
+                </button>
+              </div>
+
+            </Draggable>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
